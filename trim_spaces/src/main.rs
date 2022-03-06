c25f10c69b0f1c92c423ff98f7c80a20b0545369
Juan Tapia <juant72@gmail.com>
2022-03-06 16:54:02 -0300

    solved

--- trim_spaces/src/main.rs
+++ trim_spaces/src/main.rs
@@ -1,9 +1,24 @@
 fn main() {
     let test1 = String::from("We need more space.");
-    assert_eq!(trim_spaces(test1), String::from("We need more space."));
+    assert_eq!(trim_spaces(&test1), String::from("We need more space."));
 }
 
-fn trim_spaces(s: String) -> String {
-    let mut result = String::new();
-    result
+fn trim_spaces(s: &str) -> &str {
+    let mut start = 0;
+    for (index, character) in s.chars().enumerate() {
+        if character != ' ' {
+            start = index;
+            break;
+        }
+    }
+
+    let mut end = s.len();
+    for (index, character) in s.chars().rev().enumerate() {
+        if character != ' ' {
+            end = s.len() - index;
+            break;
+        }
+    }
+
+    &s[start..end]
 }
