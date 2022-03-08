d2d4d873820afffa5e13108e02f4fc867ac29fd7
Juan Tapia <juant72@gmail.com>
2022-03-07 23:39:29 -0300

    chellenge structs

---
+++ challenge_rectangle/src/main.rs
@@ -0,0 +1,30 @@
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
+impl Rectangle {
+    fn get_area(&self) -> f64 {
+        self.width * self.height
+    }
+
+    fn scale(&mut self, factor: f64) {
+        self.width *= factor;
+        self.height *= factor;
+    }
+
+    fn new(width: f64, height: f64) -> Rectangle {
+        Rectangle {
+            width: width,
+            height: height,
+        }
+    }
+}
+
+fn main() {
+    let mut rect = Rectangle::new(1.2, 3.4);
+    assert_eq!(rect.get_area(), 4.08);
+    rect.scale(0.5);
+    assert_eq!(rect.get_area(), 1.02);
+    println!("Test passed!");
+}
